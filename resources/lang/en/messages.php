abd7d0f383d6c0d01facfd8f3a0e402247c7acf2
reklamasieciowa <biuro@reklamasieciowa.pl>
2019-11-07 12:38:59 +0100

    translation update

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -19,7 +19,7 @@
 	<li>Personal data will be processed only for purposes related to send the Report from the National HRBP Competences Study by WNCL Sp. z o.o.</li>
 	<li>Providing this data is voluntary, but necessary to receive a report from the Study.</li>
 	<li>Use has the right to access his data, correct it and request its removal by contacting the following e-mail address: anna.grzywaczyk@wncl.pl</li>
-	<li>All data will be automatically deleted after completing the survey, i.e. January 30, 2020.</li>
+	<li>All data will be automatically deleted after completing the survey, i.e. March 30, 2020.</li>
 </ul>',
 	    '404' => 'This page cannot be found.',
 	    'back_to_home' => 'Back to the home page.',
